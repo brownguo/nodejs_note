055406f6197112452bb483a9c06927dd5464f745
YexuanGuo <yexuan.guo@gmail.com>
2022-08-05 17:09:24 +0800

    http

--- node_http.js
+++ node_http.js
@@ -1,9 +1,27 @@
-
 const http = require('http')
-
-const server = http.createServer((req, res)=>{
-    res.write("Hal;o")
-    res.end()
-}).listen(3000,()=>{
+const url = require('url')
+http.createServer((request, response)=>{
+    let req_url = request.url
+    response.writeHead(200, {
+        "Content-Type":"text/html;charset=utf-8"
+    });
+    response.end(renderHTML(req_url))
+}).listen(5000,()=>{
     console.log("server start")
 })
+
+function renderHTML(url) {
+    if(url === "/favicon.ico"){
+        return ;
+    }
+    switch (url){
+        case "/home":
+            return `Home1`
+        case "/user":
+            return `User`
+        default:
+            return `
+               not found!
+          `
+    }
+}
